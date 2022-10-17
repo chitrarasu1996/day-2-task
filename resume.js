383f311f51a9d4b794ff6c4a537c542402448fad
chitrarasu1996 <112245036+chitrarasu1996@users.noreply.github.com>
2022-10-17 11:32:40 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,28 @@
+let resume=[{
+    "Name":"Chitrarasu A",
+    "Address":"No7,Kacheri Street,Thiruvidaimaruthur(Tk),Tanjavur(Dt),pincode-612103",
+    "Mail id":"chitrarasumit@gmail.com",
+    "Career Objective":"Looking for a promising and challenging career which will enable me to provide the desktop my technical,analytical and professional skills.To pursue a career which offers an opportunity to work and contribute singnificantly to the organisation's growth ",
+    "educational Qualification":{
+        "course":"SSLC,HSC,B.com",
+        "school":"GHSS,State Board",
+        "College":"Annamalai University",
+        "Year Of Passing":"SSLC - 2011,HSC - 2013,B.com - 2022"},
+        "Eperience":"4 years of sales excutive in INDIAN Mobile at kumbakonam",
+        "Professional Strenth":"positive thinker,Handle Risky Situations,Time management",
+            
+        "personal details":{
+        "Father's Name":"Anbazhagan",
+            "Mother's Name":"Boopathi",
+            "Date Of Birth":"05.02.1996",
+            "Gender":"Male",
+            "nationality":"Indian",
+            "Religion":"Hindu",
+            "Languages Known":"Tamil,English(Speak,Read,Write)"},
+            "Declaration":"I hereby declare that all the given information are true to that best of my knowledge and belief.If I am given an oppertunity, I would do the best of your expectations.  ",
+            "phone":"8838929800"
+
+        }
+    ]
+
+console.log(resume);
